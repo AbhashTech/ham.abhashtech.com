4045000520332397d41faa70016bfaccc8e7f5ba
Kunal Gautam <kunal@ikunal.in>
2025-02-24 20:23:50 +0530

    Change Title, create Index Page

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ham.abhashtech.com
 
 go 1.24.0
+
+require github.com/McShelby/hugo-theme-relearn v0.0.0-20250223144105-3cb871d28221 // indirect
---
+++ public/searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "ASOC Exam Notes",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "ASOC Exam Notes",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
